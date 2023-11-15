b4bea4d7e6d2250291f17f13db49296bbfb04f58
SantiagoDeliaa <deliasantiago774@gmail.com>
2023-11-15 18:01:27 -0300

    add itemCount

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import "./App.css";
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
 import NavBar from "./components/NavBar/NavBar";
+import ItemCount from "./components/ItemCount/ItemCount";
+
+
 
 function App() {
   return (
     <div className="App">
       <NavBar></NavBar>
       <ItemListContainer greeting={'Hi tutor {Sofi}'}/>
+      <ItemCount initial={1} stock={10} onAdd={(quantity) => console.log('Cantidad agregada ', quantity)}/>
     </div>
   );
 }
